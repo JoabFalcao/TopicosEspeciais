5d45a3d7f6ea1a0eec79600d3cd76029ba07b693
Joab Falcão <35998391+JoabFalcao@users.noreply.github.com>
2019-04-11 08:47:03 -0300

    Update 12.py

--- AtividadeRevisão/12.py
+++ AtividadeRevisão/12.py
@@ -4,11 +4,9 @@ def valorPagamento(prestacao, atraso):
 		valor = prestacao
 	elif atraso > 0:
 		valor = prestacao + (prestacao * 0.03) + ((prestacao * 0.001) * atraso)
-	return (valor)
-	
-prestacao = float(input("Por favor, nos informe o valor da sua prestação.\nR$ "))
-
+	return (valor)	
+prestacao = float(input("Valor da prestação: "))
 while prestacao > 0:
-	atraso = int(input("Nos informe o período de atraso em dias.\n"))
-	print ("Valor final a ser pago: R$ {:.2f}\n" .format (valorPagamento(prestacao, atraso)))
-	prestacao = float(input("Por favor, nos informe o valor da sua prestação.\nR$ "))	
+	atraso = int(input("Período de dias em atraso: "))
+	print ("Valor total: R$ {:.2f}" .format (valorPagamento(prestacao, atraso)))
+	prestacao = float(input("Valor da prestação.R$ "))	
